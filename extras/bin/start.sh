84e2df82458cde5ce22d2aab99380daf0c0b20e5
Adnane Belmadiaf <adnane002@gmail.com>
2017-06-09 13:18:33 +0000

    Refactor apps Add start/stop/reload Fix snapcraft.yaml

---
+++ extras/bin/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+cd "$SNAP"
+#echo $PWD
+cp -rf conf $SNAP_DATA/
+cp -rf html $SNAP_DATA/
+cp -rf logs $SNAP_DATA/
+
+cd "$SNAP_DATA"
+#echo $PWD
+
+mkdir -p client_body_temp
+mkdir -p fastcgi_temp
+mkdir -p proxy_temp
+mkdir -p scgi_temp
+mkdir -p uwsgi_temp
+chown nobody client_body_temp
+chown nobody fastcgi_temp
+chown nobody proxy_temp
+chown nobody scgi_temp
+chown nobody uwsgi_temp
+
+# default config
+[ -e "$SNAP_USER_DATA/nginx.conf" ] || cp $SNAP/nginx.conf $SNAP_USER_DATA/nginx.conf
+
+$SNAP/bin/nginx -c $SNAP_USER_DATA/nginx.conf "$@"
+echo 'INFO: nginx started'
---
+++ extras/bin/stop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+cd "$SNAP"
+#echo $PWD
+cp -rf conf $SNAP_DATA/
+cp -rf html $SNAP_DATA/
+cp -rf logs $SNAP_DATA/
+
+cd "$SNAP_DATA"
+#echo $PWD
+
+mkdir -p client_body_temp
+mkdir -p fastcgi_temp
+mkdir -p proxy_temp
+mkdir -p scgi_temp
+mkdir -p uwsgi_temp
+chown nobody client_body_temp
+chown nobody fastcgi_temp
+chown nobody proxy_temp
+chown nobody scgi_temp
+chown nobody uwsgi_temp
+
+#kill -9 `cat $SNAP_DATA/logs/nginx.pid`
+
+$SNAP/bin/nginx -s stop
+echo 'INFO: nginx stopped'
